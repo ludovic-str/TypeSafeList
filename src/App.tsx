82da3ee6bfe3b73a7e77a08056eeb50208c6d38e
ludovic-str <ludovic.sutter@epitech.eu>
2023-02-21 11:44:16 +0100

    feat: base safe list component

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,16 @@
 import React from "react";
+import List from "./components/List";
+import "./App.css";
 
 const App = () => {
-  return <div className="App" />;
+  return (
+    <div className="App">
+      <List items={[]} />
+      <List items={[]} />
+      <List items={[]} />
+      <List items={[]} />
+    </div>
+  );
 };
 
 export default App;
---
+++ src/components/List/index.tsx
@@ -0,0 +1,29 @@
+import React from "react";
+import { v4 as uuidv4 } from "uuid";
+
+import "./styles.css";
+
+type LabelledValue<T> = {
+  label: string;
+  value: T;
+};
+
+interface ListProps<T> {
+  items: LabelledValue<T>[];
+}
+
+const List = <T,>(props: ListProps<T>) => {
+  const { items } = props;
+  return (
+    <>
+      {items.map((item) => (
+        <div className="list-card" key={uuidv4()}>
+          <h2 className="list-label">{item.label} :</h2>
+          <div className="list-value">{String(item.value)}</div>
+        </div>
+      ))}
+    </>
+  );
+};
+
+export default List;
